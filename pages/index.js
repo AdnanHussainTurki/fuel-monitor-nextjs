fa55d8b7f2b5c1c9057c5d9b1894675990b84159
Adnan Hussain Turki <adnanhussainturki@gmail.com>
2023-01-04 00:37:26 +0530

    Revert "Initial Commit"
    This reverts commit 95dafa8615d4eddbc9a3a9b2d2fe7ab4424f8e25.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { RiWaterFlashFill } from 'react-icons/ri'
+import { HiPlusCircle } from 'react-icons/hi2'
 import Auth from '../src/components/Templates/Auth/Auth'
 import SmallVehicle from '../src/components/Vehicle/SmallVehicle'
 import { useRef, useState, useEffect } from 'react'
@@ -8,60 +8,37 @@ import { getSession } from 'next-auth/react'
 import { fetchData } from 'next-auth/client/_utils'
 import Loading from '../src/components/Layout/Loading/Loading'
 import VehicleStore from '../src/stores/VehicleStore'
-import { useWeb3React } from '@web3-react/core'
-import withIsMobile from '../src/components/Layout/NavBar/withIsMobile'
-import withWalletSelectModal from '../src/components/Layout/NavBar/withWalletSelectModal'
-import { walletLogin } from '../src/utils/account'
-import ConnectWithMetamask from '../src/components/Button/ConnectWithMetamask'
-import { useStoreState } from 'pullstate'
-import AccountStore from '../src/stores/AccountStore'
-import { displayCurrencyWithDecimalsNonWaiting } from '../src/utils/math'
-
-const Home = ({ showLogin, connect }) => {
-    const { active, account, activate } = useWeb3React()
-    const balances = useStoreState(AccountStore, (s) => s.balances);
-    const water = useStoreState(AccountStore, (s) => s.water);
-    const compromiseds = useStoreState(AccountStore, (s) => s.compromised);
-    const [compromised, setCompromised] = useState([])
+export default function Home(props) {
     const [isLoading, setIsLoading] = useState(true)
-    const accountStore = AccountStore.useState((s) => s)
+    const [vehicles, setVehicles] = useState([])
+    const vehicleStore = VehicleStore.useState((s) => s)
 
     const router = useRouter()
     useEffect(() => {
         getSession().then((session) => {
             if (!session) {
                 router.replace('/auth/signin')
-                return
+                return;
             }
         })
     }, [router])
     useEffect(() => {
-        if (balances["eth"] != undefined && water) {
+        const fetchVehicles = async () => {
+            const response = await fetch('/api/vehicle/list')
+            const data = await response.json()
+            VehicleStore.update((s) => {
+                (s.vehicles = data.vehicles), (s.vehicleRefreshNeeded = false)
+            })
+            setVehicles(data.vehicles)
             setIsLoading(false)
         }
-    }, [water, balances])
-
-    useEffect(() => {
-        const fetchCompromised = async () => {
-            const response = await fetch('/api/compromised/list')
-            const data = await response.json()
-            AccountStore.update((s) => {
-                (s.compromised = data.compromiseds), (s.compromisedRefreshNeeded = false)
-            })
-            setCompromised(data.compromiseds)
-            // setIsLoading(false)
-        }
-        if (accountStore.compromisedRefreshNeeded === true) {
-          fetchCompromised()
+        if (vehicleStore.vehicleRefreshNeeded === true) {
+          fetchVehicles()
         } else {
-            if (accountStore.compromised) {
-                return;
-            }
-            setCompromised(accountStore.compromised)
-            // setIsLoading(false)
+          setVehicles(vehicleStore.vehicles)
+          setIsLoading(false)
         }
     }, [])
-
 
     return (
         <Auth>
@@ -69,64 +46,51 @@ const Home = ({ showLogin, connect }) => {
                 <div class="p-6 bg-white border-b ">
                     <div class="flex flex-row">
                         <div class="basis-1/2">
-                            {active && !isLoading && (
-                                <span>
-                                    <span className="font-bold">Account:</span>
-                                    <br />{' '}
-                                    <span className="text-red-600">
-                                        {account}
-                                    </span>
-                                </span>
-                            )}
+                            {/* <span>Portfolio: </span>
+                            <span class="text-emerald-500 bg-emerald-500/5 text-sm px-3 py-2 inline-flex gap-2 items-center justify-center">
+                                <span class="font-mono">Rs. 61,334.00</span>
+                            </span> */}
                         </div>
                         <div class="basis-1/2 align-middle text-center ">
-                            {!active && (
-                                <ConnectWithMetamask walletLogin={walletLogin} showLogin={showLogin} activate={activate} />
-                            )}
-                            {active && !isLoading && (
-                                <span>
-                                    <span className="font-bold">
-                                        ETH Balance:
-                                    </span>
-                                    <br />{' '}
-                                    <span className="text-red-600">
-                                        {balances["eth"].display} {balances["eth"].name}
-                                    </span>
-                                </span>
-                            )}
-                            <br />
-                            {active && !isLoading && (
-                                <span>
-                                    <span className="font-bold">
-                                        Water in FireTruck:
-                                    </span>
-                                    <br />{' '}
-                                    <span className="text-red-600">
-                                        { displayCurrencyWithDecimalsNonWaiting(water, 18)} {balances["eth"].name}
-                                    </span>
-                                </span>
-                            )}
+                            <Link
+                                href={'/vehicle/add'}
+                                class="text-red-400 bg-white hover:bg-red-100 border border-red-200 focus:ring-4 focus:outline-none focus:ring-red-100 font-medium rounded-lg text-sm px-4 py-2 text-center inline-flex items-center dark:focus:ring-red-600 dark:bg-red-600 dark:border-red-500 dark:text-white dark:hover:bg-red-500 mr-2 mb-2"
+                            >
+                                Add Vehicle
+                            </Link>
                         </div>
                     </div>
                 </div>
             </div>
             <br />
-            {active && isLoading && <Loading />}
-
-            {active && !isLoading && water == 0 && (
+            {isLoading && <Loading />}
+            {!isLoading &&
+                vehicles.map((vehicle) => {
+                    return (
+                        <div class="flex flex-wrap flex-gap-3 justify-center">
+                            <Link
+                                href="/"
+                                class="p-4 cursor-pointer hover:scale-105 transition duration-300 ease-in-out"
+                            >
+                                <SmallVehicle vehicle={vehicle} />
+                            </Link>
+                        </div>
+                    )
+                })}
+            {!isLoading && vehicles.length == 0 && (
                 <Link
-                    href={'/mission-control/fill-water'}
+                    href={'/vehicle/add'}
                     class="flex flex-wrap flex-gap-3 justify-center"
                 >
                     <div class="p-4 cursor-pointer hover:scale-105 transition duration-300 ease-in-out">
                         <div class="flex justify-center">
                             <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
-                                <RiWaterFlashFill className="w-30 font-large fill-red-500" />
+                                <HiPlusCircle className="h-6 fill-red-500" />
                                 <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">
                                     <span class="text-red-600 text-xl font-bold">
                                         Add
                                     </span>{' '}
-                                    Water in FireTruck
+                                    Vehicle
                                 </h5>
                                 <p class="text-gray-700 text-base">
                                     Add your first vehicle to get started.
@@ -136,99 +100,20 @@ const Home = ({ showLogin, connect }) => {
                     </div>
                 </Link>
             )}
-            {active && !isLoading && water > 0 && (
-                <div>
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg ml-4 mr-4">
-                        <div class="p-6 bg-white border-b ">
-                            <div class="flex flex-row place-content-end">
-                                <Link
-                                    href={'/mission-control/fill-water'}
-                                    className="self-end text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-                                >
-                                    Fill Water
-                                </Link>
-                                <Link
-                                    href={'/compromised/add'}
-                                    className="self-end text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-                                >
-                                    Add Compromised Account
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="my-7 bg-white overflow-hidden shadow-sm sm:rounded-lg ml-4 mr-4">
-                        <div class="p-6 bg-white border-b ">
-                            <div class="flex flex-row">
-
-
-                                <div class="overflow-x-auto relative shadow-md sm:rounded-lg w-full">
-                                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                            <tr>
-                                                <th scope="col" class="py-3 px-6">
-                                                    Name
-                                                </th>
-                                                <th scope="col" class="py-3 px-6">
-                                                    Tokens
-                                                </th>
-
-                                                <th scope="col" class="py-3 px-6">
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {compromiseds.map((compromised, index) => (
-                                                (
-                                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                                        <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                            {compromised.name}
-                                                        </th>
-                                                        <td class="py-4 px-6">
-                                                            Sliver
-                                                        </td>
-                                                        <td class="py-4 px-6">
-                                                            <div class="flex flex-row place-content-end">
-                                                                <Link
-                                                                    href={`compromised/view/${compromised._id}`}
-                                                                    className="self-end text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-3 py-1.5 text-center mr-2 mb-2"
-                                                                >
-                                                                    View
-                                                                </Link>
-                                                            </div>
-                                                        </td>
-                                                    </tr>
-                                                )
-                                            ))}
-
-
-                                        </tbody>
-                                    </table>
-                                </div>
-
-
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            )}
         </Auth>
     )
 }
 export async function getServerSideProps(context) {
-    const session = await getSession({ req: context.req })
-    if (!session) {
-        return {
-            redirect: {
-                destination: '/auth/signin',
-                permanent: false,
-            },
-        }
-    }
-    return {
-        props: { session },
-    }
+  const session = await getSession({ req: context.req })
+  if (!session) {
+      return {
+          redirect: {
+              destination: '/auth/signin',
+              permanent: false,
+          },
+      }
+  }
+  return {
+      props: { session },
+  }
 }
-
-export default withIsMobile(withWalletSelectModal(Home))
